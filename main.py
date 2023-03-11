cd87de5007aa0b28ffe0399fca0e5269189983cc
alxjsndc <126742649+alxjsndc@users.noreply.github.com>
2023-03-11 01:20:16 +0000

    Complete E4

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+  ##################################################
+  # Comlete your code here
+  ##################################################
+  celcius = int(input('Enter the Celsius Degree: '))
+  fahrenheit = (9.0 / 5.0) * celcius + 32
+  print(f'The Fahrenheit is {fahrenheit:.2f}')
+
+  pass
 
 
 if __name__ == '__main__':
     main()
+  
